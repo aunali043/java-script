44835f47e01f89975bb4eb02db55b22ae00e572c
aunali043 <aunaliumatiya1286@gmail.com>
2025-02-05 05:00:20 +0000

    main

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14558 
+let accountEmail = "xyz123@gmail.com"
+var accountpassword = "1568"
+accountcity = "Ahmedabad"
+let accountstate =
+
+// accountId = 2 not allowed without using const,let or var
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountpassword,accountcity,accountstate])
